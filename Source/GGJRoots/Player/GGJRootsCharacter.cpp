f2ce14d704e17865a9e1ef6e6926f0ba71d78289
RadoslawDorociak <115507756+RadoslawDorociak@users.noreply.github.com>
2023-02-04 18:33:02 +0000

    jump

--- Source/GGJRoots/Player/GGJRootsCharacter.cpp
+++ Source/GGJRoots/Player/GGJRootsCharacter.cpp
@@ -30,7 +30,7 @@ AGGJRootsCharacter::AGGJRootsCharacter()
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->JumpZVelocity = 500.f;
 	GetCharacterMovement()->AirControl = 0.35f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
